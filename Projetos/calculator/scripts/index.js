a098a80a60750da9a7cf56c9999fb24285a9f7f2
Hugo Almeida <116381112+hugoalmeida02@users.noreply.github.com>
2025-02-08 21:55:26 +0000

    Finish calculator project

--- Projetos/calculator/scripts/index.js
+++ Projetos/calculator/scripts/index.js
@@ -1,15 +1,51 @@
 const display = document.querySelector("#display");
 
+let result = ``;
+let on
+
 document.querySelectorAll(".button").forEach((link) => {
-    
   link.addEventListener("click", () => {
-    const value = link.dataset.value
-    if (value === "delete") {
-        display.innerHTML = ``;
+    const value = link.dataset.value;
+
+    if (value === "clear") {
+      clearDisplay(display);
+      result = ``;
     } else if (value === "calculate") {
-        calculate(display.innerHTML)
-    } else {
-      display.innerHTML += `${value}`;
-    } 
-  }); 
+      calculate(result, display);
+      on = true
+    } else if (on) {
+      on = false
+      clearDisplay(display);
+      result = ``;
+      if (value === "^") {
+        display.innerHTML += `${value}`;
+        result += `**`;
+      } else {
+        display.innerHTML += `${value}`;
+        result += `${value}`;
+      }
+    }else{
+      if (value === "^") {
+        display.innerHTML += `${value}`;
+        result += `**`;
+      } else {
+        display.innerHTML += `${value}`;
+        result += `${value}`;
+      }
+    }
+  });
 });
+
+function calculate(result, display) {
+  try {
+    result = eval(result);
+    display.innerHTML = `${result}`;
+    console.log(result);
+  } catch (error) {
+    display.innerHTML = "Error";
+  }
+}
+
+function clearDisplay(display) {
+  display.innerHTML = ``;
+}
