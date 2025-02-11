e47e0c32adbfad80df41c9d4ac411d674fec9f9b
Hugo Almeida <116381112+hugoalmeida02@users.noreply.github.com>
2025-02-11 00:29:08 +0000

    Start rpg project
    https://www.youtube.com/watch?v=nu_pCVPKzTk&list=PLWKjhJtqVAbn21gs5UnLhCQ82f923WCgM&index=2&ab_channel=freeCodeCamp.org

---
+++ Projetos/rpg/scripts/index.js
@@ -0,0 +1,5 @@
+const button1 = document.querySelector("#button1")
+const button2 = document.querySelector("#button1")
+const button3 = document.querySelector("#button1")
+const text = document.querySelector("#text")
+
